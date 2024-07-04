f44f6b75368fa744f5cab453be3d21841e0aeb87
PFercovich <pedro.fercovich@gmail.com>
2024-07-04 16:20:10 -0500

    recorrido diccionarios

---
+++ dia1/09-recorrido-diccionarios.py
@@ -0,0 +1,28 @@
+capitales = {
+  'Perú':'Lima',
+  'Ecuador':'Quito',
+  'Chile':'Santiago',
+  'Colombia':'Bogota'
+}
+
+#recorrido por claves
+for clave in capitales.keys():
+  print(clave)
+
+print('*'*50)
+
+#recorrido por valores
+for valor in capitales.values():
+  print(valor)
+
+print('*'*50)
+
+#recorrido por clave valor
+for clave,valor in capitales.items():
+  print(f'la capital de {clave} es {valor}')
+
+print('*'*50)
+
+#DICTIONARY COMPREHENSION
+paises = {valor:clave for clave,valor in capitales.items()}
+print(paises)
