46c2a697f87469420f9e045935102c729372bb84
PFercovich <pedro.fercovich@gmail.com>
2024-07-04 13:39:46 -0500

    monedas_while.py

--- dia1/03-bucles.py
+++ dia1/03-bucles.py
@@ -2,3 +2,8 @@ tabla = int(input("INGRESE LA TABLA A MULTIPLICAR: "))
 
 for contador in range(1,13,2):
   print(f"{tabla} x {contador} = {tabla * contador}")
+
+contador = 1
+while(contador <=12):
+  print(f"{tabla} x {contador} = {tabla * contador}")
+  contador += 1
---
+++ dia1/monedas_while.py
@@ -0,0 +1,27 @@
+# CONVERTIR DE SOLES A DÓLARES
+# CONVERTIR DE DÓLARES A SOLES
+# ENTRADAS
+TIPO_CAMBIO_COMPRA = 3.834
+TIPO_CAMBIO_VENTA = 3.851
+opcion = 0
+while(opcion != 1 and opcion != 2):
+  print("CONVERTIDOR DE MONEDAS 1.0")
+  print("""
+        [1] CONVERTIR SOLES A DÓLARES
+        [2] CONVERTIR DÓLARES A SOLES
+        """)
+  opcion = int(input("ELIJA UNA OPCIÓN: "))
+  if(opcion == 1):
+    monto_origen = float(input("INGRESE EL MONTO EN SOLES: "))
+    #PROCESO
+    monto_destino = monto_origen / TIPO_CAMBIO_VENTA
+    #SALIDA
+    print(f"EL MONTO EN DÓLARES ES {monto_destino}")
+  elif(opcion == 2):
+    monto_origen = float(input("INGRESE EL MONTO EN DÓLARES: "))
+    #PROCESO
+    monto_destino = monto_origen * TIPO_CAMBIO_COMPRA
+    #SALIDA
+    print(f"EL MONTO EN SOLES ES {monto_destino}")
+  else:
+    print("OPCIÓN NO VÁLIDA")
